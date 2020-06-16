d880c985ad74be8280006346f87287676bdc946b
Fabio Junior Policeno <admin@policeno.com>
2020-06-16 15:25:36 -0400

    register screen finished and added overlay masks

--- app/screens/ListingsScreen.jsx
+++ app/screens/ListingsScreen.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
-import Screen from '../components/Screen';
+import ActivityIndicator from '../components/ActivityIndicator';
+import AppButton from '../components/AppButton';
 import Card from '../components/Card';
 import colors from '../config/colors';
 import listingsApi from '../api/listings';
 import routes from '../navigation/routes';
+import Screen from '../components/Screen';
 import AppText from '../components/AppText';
-import AppButton from '../components/AppButton';
-import ActivityIndicator from '../components/ActivityIndicator';
 import useApi from '../hooks/useApi';
 
 function ListingsScreen({ navigation }) {
@@ -21,28 +21,31 @@ function ListingsScreen({ navigation }) {
   }, []);
 
   return (
-    <Screen style={styles.screen}>
-      {error && (
-        <>
-          <AppText>Couldn't retrieve the listings.</AppText>
-          <AppButton text='Retry' onPress={loadListings} />
-        </>
-      )}
+    <>
       <ActivityIndicator visible={loading} />
-      <FlatList
-        data={listings}
-        keyExtractor={(listing) => listing.id.toString()}
-        renderItem={({ item }) => (
-          <Card
-            title={item.title}
-            subTitle={`$${item.price}`}
-            imageUrl={item.images[0].url}
-            thumbnailUrl={item.images[0].thumbnailUrl}
-            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-          />
+      <Screen style={styles.screen}>
+        {error && (
+          <>
+            <AppText>Couldn't retrieve the listings.</AppText>
+            <AppButton text='Retry' onPress={loadListings} />
+          </>
         )}
-      />
-    </Screen>
+
+        <FlatList
+          data={listings}
+          keyExtractor={(listing) => listing.id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subTitle={`$${item.price}`}
+              imageUrl={item.images[0].url}
+              thumbnailUrl={item.images[0].thumbnailUrl}
+              onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+            />
+          )}
+        />
+      </Screen>
+    </>
   );
 }
 
--- app/screens/RegisterScreen.jsx
+++ app/screens/RegisterScreen.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
-import { AppFormField, SubmitButton, AppForm } from '../components/forms';
+import usersApi from '../api/users';
+import authApi from '../api/auth';
+import useAuth from '../auth/useAuth';
+import {
+  ErrorMessage,
+  AppFormField,
+  SubmitButton,
+  AppForm,
+} from '../components/forms';
+import useApi from '../hooks/useApi';
+import ActivityIndicator from '../components/ActivityIndicator';
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().min(4).label('Name'),
@@ -11,41 +21,71 @@ const validationSchema = Yup.object().shape({
   password: Yup.string().required().min(4).label('Password'),
 });
 
-function RegisterScreen(props) {
+function RegisterScreen() {
+  const registerApi = useApi(usersApi.register);
+  const loginApi = useApi(authApi.login);
+  const auth = useAuth();
+  const [error, setError] = useState();
+
+  const handleSubmit = async (userInfo) => {
+    const result = await registerApi.request(userInfo);
+
+    if (!result.ok) {
+      if (result.data) {
+        setError(result.data.error);
+      } else {
+        setError('An unexpected error ocurred');
+        console.log(result);
+      }
+      return;
+    }
+
+    const { data: authToken } = await loginApi.request(
+      userInfo.email,
+      userInfo.password
+    );
+
+    auth.logIn(authToken);
+  };
+
   return (
-    <Screen style={styles.container}>
-      <AppForm
-        initialValues={{ name: '', email: '', password: '' }}
-        onSubmit={(values) => console.log(values)}
-        validationSchema={validationSchema}
-      >
-        <AppFormField
-          name='name'
-          autoCorrect={false}
-          icon='account'
-          placeholder='Name'
-        />
-        <AppFormField
-          name='email'
-          autoCapitalize='none'
-          autoCorrect={false}
-          icon='email'
-          keyboardType='email-address'
-          placeholder='Email'
-          textContentType='emailAddress'
-        />
-        <AppFormField
-          name='password'
-          autoCapitalize='none'
-          autoCorrect={false}
-          icon='lock'
-          placeholder='Password'
-          secureTextEntry
-          textContentType='password'
-        />
-        <SubmitButton text='Register' />
-      </AppForm>
-    </Screen>
+    <>
+      <ActivityIndicator visible={registerApi.loading || loginApi.loading} />
+      <Screen style={styles.container}>
+        <AppForm
+          initialValues={{ name: '', email: '', password: '' }}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}
+        >
+          <ErrorMessage error={error} visible={error} />
+          <AppFormField
+            name='name'
+            autoCorrect={false}
+            icon='account'
+            placeholder='Name'
+          />
+          <AppFormField
+            name='email'
+            autoCapitalize='none'
+            autoCorrect={false}
+            icon='email'
+            keyboardType='email-address'
+            placeholder='Email'
+            textContentType='emailAddress'
+          />
+          <AppFormField
+            name='password'
+            autoCapitalize='none'
+            autoCorrect={false}
+            icon='lock'
+            placeholder='Password'
+            secureTextEntry
+            textContentType='password'
+          />
+          <SubmitButton text='Register' />
+        </AppForm>
+      </Screen>
+    </>
   );
 }
 
