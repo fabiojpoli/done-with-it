44ba54d0d993d8c7bab3a8a2d9a4b1374ddf1318
Fabio Junior Policeno <admin@policeno.com>
2020-06-17 00:38:55 -0400

    config env and platform

---
+++ app/config/settings.js
@@ -0,0 +1,27 @@
+import Constants from 'expo-constants';
+
+const settings = {
+  dev: {
+    apiUrl: 'http://192.168.86.23:9000/api',
+  },
+  staging: {
+    apiUrl: 'http://192.168.86.23:9000/api',
+  },
+  prod: {
+    apiUrl: 'http://192.168.86.23:9000/api',
+  },
+};
+
+const getCurrentSettings = () => {
+  if (__DEV__) {
+    return settings.dev;
+  }
+
+  if (Constants.manifest.releaseChannel === 'staging') {
+    return settings.staging;
+  }
+
+  return settings.prod;
+};
+
+export default getCurrentSettings();
